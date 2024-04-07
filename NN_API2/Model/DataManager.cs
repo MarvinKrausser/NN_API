102113b059d7ea488a58f08a77a7887f18dd3164
sgc40 <marvin.krausser@uni-ulm.de>
2024-04-07 20:09:51 +0200

    first commit

---
+++ NN_API2/Model/DataManager.cs
@@ -0,0 +1,54 @@
+﻿namespace NN_API.Model;
+
+public class DataManager
+{
+    public static void AddData(string array)
+    {
+        string result = string.Empty;
+        foreach (var c in array)
+        {
+            if (char.IsDigit(c))
+            {
+                result += c;
+            }
+        }
+
+
+        using StreamWriter writer = new StreamWriter(Directory.GetCurrentDirectory() + "\\data.txt", append: true);
+        writer.Write("$" + result);
+    }
+    
+    public static (List<int>, List<List<int>>) GetData()
+    {
+        List<List<int>> array = new List<List<int>>();
+        List<int> digits = new List<int>();
+        using StreamReader reader = new StreamReader( Directory.GetCurrentDirectory() + "\\data.txt");
+        int charCode;
+        bool nextIsDigit = false;
+        int counter = -1;
+
+        // Read character by character until end of file
+        while ((charCode = reader.Read()) != -1)
+        {
+            char character = (char)charCode;
+
+            if (character == '$')
+            {
+                array.Add(new List<int>());
+                counter++;
+                nextIsDigit = true;
+            }
+            else if (nextIsDigit)
+            {
+                digits.Add(character - 48);
+                nextIsDigit = false;
+            }
+            else
+            {
+                array[counter].Add(character - 48);
+            }
+        }
+
+        return (digits, array);
+    }
+}
---
+++ NN_API2/Model/NeuralNetworkManager.cs
@@ -0,0 +1,75 @@
+﻿using NeuralNetworkNET.APIs;
+using NeuralNetworkNET.APIs.Enums;
+using NeuralNetworkNET.APIs.Interfaces;
+using NeuralNetworkNET.APIs.Interfaces.Data;
+using NeuralNetworkNET.APIs.Results;
+using NeuralNetworkNET.APIs.Structs;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace NN_API.Model;
+
+public class NeuralNetworkManager
+{
+    private static INeuralNetwork network = CreateNn();
+    public static  void Train()
+    {
+        var dataRaw = DataManager.GetData();
+        var targets = new List<float[]>();
+
+        List<(float[], float[])> data = new List<(float[], float[])>();
+
+        for (int i = 0; i < dataRaw.Item1.Count; i++)
+        {
+            data.Add((dataRaw.Item2[i].Select(j => (float)j).ToArray(), OneHotEncode(dataRaw.Item1[i])));
+        }
+        
+        Console.WriteLine(data.Count);
+        
+        ITrainingDataset dataset = DatasetLoader.Training(data, 100);
+        
+        TrainingSessionResult result = NetworkManager.TrainNetwork(
+            network,                                // The network instance to train
+            dataset,                                // The ITrainingDataset instance   
+            TrainingAlgorithms.AdaDelta(),          // The training algorithm to use
+            60,                                     // The expected number of training epochs to run
+            0.5f,                                   // Dropout probability
+            null,                                   // Optional callback to monitor the training dataset accuracy
+            null);                                 // Cancellation token for the training
+        Console.WriteLine(result.StopReason);
+    }
+    
+    public static int Predicate(int[] input)
+    {
+        return IndexOfMax(network.Forward(input.Select(i => (float)i).ToArray()));
+    }
+    
+    private static int IndexOfMax(float[] array)
+    {
+        if (array == null || array.Length == 0)
+            throw new ArgumentException("Array is null or empty");
+
+        float maxValue = array.Max();
+        return Array.IndexOf(array, maxValue);
+    }
+    
+    private static float[] OneHotEncode(int digit)
+    {
+        if (digit < 0 || digit > 9)
+        {
+            throw new ArgumentException("Digit must be between 0 and 9");
+        }
+
+        float[] encodedArray = new float[10];
+        encodedArray[digit] = 1;
+        return encodedArray;
+    }
+
+    private static INeuralNetwork CreateNn()
+    {
+        return NetworkManager.NewSequential(TensorInfo.Image<Alpha8>(30, 30),
+            NetworkLayers.Convolutional((5, 5), 20, ActivationType.Identity),
+            NetworkLayers.Pooling(ActivationType.LeakyReLU),
+            NetworkLayers.FullyConnected(100, ActivationType.LeCunTanh),
+            NetworkLayers.Softmax(10));
+    }
+}
