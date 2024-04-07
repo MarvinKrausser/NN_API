4f7b62d01d80a6a0c22cfe6738ade04dcad3e476
sgc40 <marvin.krausser@uni-ulm.de>
2024-04-07 20:11:29 +0200

    third commit

---
+++ NN_API2/Controllers/DigitNnController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NN_API.Model;
+
+namespace NN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DigitNnController : ControllerBase
+    {
+        // GET: api/DigitNn
+        [HttpGet]
+        public int Get(string data)
+        {
+            return NeuralNetworkManager.Predicate(JsonConvert.DeserializeObject<int[]>(data));
+        }
+
+        // GET: api/DigitNn/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/DigitNn
+        [HttpPost]
+        public void Post()
+        {
+            NeuralNetworkManager.Train();
+        }
+
+        // PUT: api/DigitNn/5
+        [HttpPut]
+        public void Put(string data)
+        {
+            DataManager.AddData(data);
+        }
+
+        // DELETE: api/DigitNn/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
